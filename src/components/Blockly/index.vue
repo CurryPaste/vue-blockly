2806cbfac7cd0dfb3614f210a66a58204cc20865
susheng <lisusheng@dycjr.com>
2022-12-13 15:53:09 +0800

    feat: 基础框架更改

---
+++ src/components/Blockly/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container">
+    <el-table
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    />
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+  },
+  methods: {
+  }
+}
+</script>
---
+++ src/views/componentManage/blockly.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>这是图形编程</span>
+    <blocklyCom />
+  </div>
+</template>
+
+<script>
+import blocklyCom from '@/components/Blockly/index.vue'
+export default {
+  name: 'Blockly',
+  components: {
+    blocklyCom
+  }
+
+}
+</script>
+
+<style>
+
+</style>
